d96825c3a21c804e561fcb2d631999ef8c82822b
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:28:47 +0530

    Add files via upload

---
+++ Assignment 1/GoodMorning.java
@@ -0,0 +1,19 @@
+class GoodMorning{
+
+              public static void main(String args[]){
+
+                   int time = 9;
+
+                   if (time >5 && time<=12) {
+
+                       System.out.println("Good Morning");
+
+            }      
+                    else {
+                       System.out.println("It is not morning");
+                       
+       }
+
+   }
+
+}
