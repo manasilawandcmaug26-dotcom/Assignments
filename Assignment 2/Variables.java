ab5d15c7964acf0428db3e96ba3c06c8f58eee54
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:33:55 +0530

    Add files via upload

---
+++ Assignment 2/Variables.java
@@ -0,0 +1,24 @@
+class Variables{
+      public static void main(String args[]){
+ 
+      byte a = 0;
+      short b = 0;
+      int c = 0;     
+      long d = 0L;
+      float e = 0.0f;
+      double f = 00.00;
+      char g = 'D';
+      boolean h = false;
+
+
+      System.out.println("byte a:" +a);
+      System.out.println("short b:" +b);
+      System.out.println("int c:" +c);
+      System.out.println("long d:" +d);
+      System.out.println("float e:" +e);
+      System.out.println("double f:" +f);
+      System.out.println("char g:" +g);
+      System.out.println("boolean h:" +h);
+  }
+
+}
