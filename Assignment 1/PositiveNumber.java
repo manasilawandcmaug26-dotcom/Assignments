95aba9c2e8bfda293cfbf07fa49b85a9713eb968
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:27:01 +0530

    Add files via upload

---
+++ Assignment 1/PositiveNumber.java
@@ -0,0 +1,19 @@
+class PositiveNumber{
+
+              public static void main(String args[]){
+
+                   int num = 4;
+
+                   if (num > 0) {
+
+                       System.out.println("The number is positive");
+
+            }      
+                    else {
+                       System.out.println("The number is not positive");
+                       
+       }
+
+   }
+
+}
