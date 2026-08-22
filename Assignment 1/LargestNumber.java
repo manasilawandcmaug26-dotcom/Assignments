be027c027c4e7c267642f4acdf80e3b06ab93a1a
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:30:17 +0530

    Add files via upload

---
+++ Assignment 1/LargestNumber.java
@@ -0,0 +1,26 @@
+class LargestNumber{
+
+              public static void main(String args[]){
+
+                   int a = 10;
+                   int b = 5;
+                   int c = 20;
+
+                   if (a >= b && a >= c) {
+
+                       System.out.println("a is largest number");
+
+            }      
+                   else if(b >= a && b>=c ){
+                       System.out.println("b is largest number");
+                       
+            } 
+                   else{
+                      System.out.println("c is largest number");
+ 
+
+         }
+
+   }
+
+}
