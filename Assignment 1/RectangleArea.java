14fc5297676f6c437294eba739c5f9a5e5b3eeda
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:29:37 +0530

    Add files via upload

---
+++ Assignment 1/RectangleArea.java
@@ -0,0 +1,14 @@
+class RectangleArea{
+
+              public static void main(String args[]){
+
+                   double length = 10;
+                   double width = 5;
+
+                   double area = length * width;               
+ 
+                   System.out.println("Area of rectangle:" +area);
+
+   }
+
+}
