15bbbbbe7c07ac1d4d0c3f391b5e24730621a1e9
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:29:14 +0530

    Add files via upload

---
+++ Assignment 1/SquareArea.java
@@ -0,0 +1,13 @@
+class SquareArea{
+
+              public static void main(String args[]){
+
+                   double side = 4;
+
+                   double area = side*side;               
+ 
+                   System.out.println("Area of square:" +area);
+
+   }
+
+}
