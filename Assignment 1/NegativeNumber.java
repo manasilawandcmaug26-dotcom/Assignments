d0079bbc28c5a6dbde1c9a040c9213149c4d170c
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:27:46 +0530

    Add files via upload

---
+++ Assignment 1/NegativeNumber.java
@@ -0,0 +1,19 @@
+class NegativeNumber{
+
+              public static void main(String args[]){
+
+                   int num = -2;
+
+                   if (num < 0) {
+
+                       System.out.println("The number is negative");
+
+            }      
+                    else {
+                       System.out.println("The number is not negative");
+                       
+       }
+
+   }
+
+}
