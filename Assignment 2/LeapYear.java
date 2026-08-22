72fd29a5c35f0cd8654e70f9854282782025d870
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:33:07 +0530

    Add files via upload

---
+++ Assignment 2/LeapYear.java
@@ -0,0 +1,30 @@
+class LeapYear {
+
+    public static void main(String[] args) {
+
+
+        int year = 2023;
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+
+            System.out.println(year + " is a Leap Year");
+
+        } else {
+
+            System.out.println(year + " is Not a Leap Year");
+        }
+
+
+        year = 2028;
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+
+            System.out.println(year + " is a Leap Year");
+
+        } else {
+
+            System.out.println(year + " is Not a Leap Year");
+        }
+
+    }
+}
