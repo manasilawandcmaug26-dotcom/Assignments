4b32d22c25f3eef4b7af5267cab51f9e08402acb
manasilawandcmaug26-dotcom <manasi.lawand.cmaug26@gmail.com>
2026-08-22 19:35:55 +0530

    Add files via upload

---
+++ Assignment 3/EvenNumbers.java
@@ -0,0 +1,22 @@
+class EvenNumbers {
+
+       public static void printEvenNumbers() {
+        
+        int number = 1;
+
+        while (number <= 50) {
+           
+            if (number % 2 == 0) {
+                System.out.print(number + " ");
+            }
+            
+            number++;
+        }
+        System.out.println(); 
+    }
+
+  
+       public static void main(String[] args) {
+             printEvenNumbers();
+    }
+}
